d2937db5ec51c6dde81ca7af6ab110e831a3cb37
delaneybarrow <delaneybarrow19@gmail.com>
2018-06-08 16:57:33 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # dog.rb
+class Dog 
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+  
+  def name
+    @this_dogs_name
+  end
+end
+
+fido = Dog.new 
+snoopy = Dog.new 
+lassie = Dog.new
+fido.name 
+snoopy.name
+lassie.name
+
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,14 @@
 # person.rb
+class Person 
+  def name=(person_name)
+    @this_persons_name = person_name
+  end
+  def name 
+    @this_persons_name
+  end
+end
+
+adele_goldberg = Person.new 
+alan_kay = Person.new 
+adele_goldberg.name
+alan_kay.name
